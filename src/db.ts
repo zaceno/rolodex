7ec67f2a545a970633ff0b98b9e6aa91ca5516f1
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2023-02-14 15:27:13 +0100

    sync users from api to indexeddb

---
+++ src/db.ts
@@ -0,0 +1,162 @@
+/*
+-------------------------------------------------------------
+
+The purpose of this module is to maintain a local database
+of contacts, synced from the API, and mapped to local format,
+in order to be able to search and sort the contacts without
+going over the request-throttling-threshold of the API.
+
+We sync local DB with the API at most once every 10 minutes. 
+
+
+-------------------------------------------------------------
+*/
+
+const DB_NAME = "zach-rolodex"
+const DB_VERSION = 1
+const DB_OBJ_STORE = "people"
+const API_URL = "https://randomuser.me/api/"
+const API_NRESULTS = 5000
+const API_SEED = "zach"
+const LS_LAST_SYNC_KEY = "zach-rolodex-last-sync"
+const TEN_MINUTES_MS = 1000 * 60 * 10
+
+// Person such as the API provides them, in order to perform type-safe mapping
+type APIPerson = {
+  name: { first: string; last: string; title: string }
+  login: { uuid: string }
+  location: {
+    street: {
+      number: number
+      name: string
+    }
+    postcode: number
+    city: string
+    country: string
+  }
+  email: string
+  phone: string
+  cell: string
+  picture: {
+    large: string
+    thumbnail: string
+  }
+}
+
+// Person such as we store in local database
+export type Person = {
+  id: string
+  firstname: string
+  lastname: string
+  title: string
+  phone1: string
+  phone2: string
+  address1: string
+  address2: string
+  address3: string
+  email: string
+  thumbnail: string
+  portrait: string
+}
+
+/**
+ * Return a handle to the IndexedDB, while making sure the
+ * object store exists.
+ */
+function getDB(): Promise<IDBDatabase> {
+  let resolve: (db: IDBDatabase) => any,
+    reject: (err: Event) => any,
+    promise = new Promise<IDBDatabase>((r, j) => {
+      resolve = r
+      reject = j
+    })
+
+  // Ensure the object store exists
+  const dbreq = indexedDB.open(DB_NAME, DB_VERSION)
+  dbreq.onupgradeneeded = ev => {
+    const db = dbreq.result
+    const store = db.createObjectStore(DB_OBJ_STORE, { keyPath: "id" })
+    store.createIndex("firstname", "firstname", { unique: false })
+    store.createIndex("lastname", "lastname", { unique: false })
+  }
+  dbreq.onsuccess = () => resolve(dbreq.result)
+  dbreq.onerror = ev => reject(ev)
+  return promise
+}
+
+/**
+ * Fetches the current contact list from the API, in the
+ * format they use.
+ */
+function fetchAPIPersons(): Promise<APIPerson[]> {
+  return fetch(`${API_URL}?results=${API_NRESULTS}&seed=${API_SEED}`)
+    .then(x => x.json())
+    .then(x => x.results as APIPerson[])
+}
+
+/**
+ * Transform a contact in the API format to a
+ * person on the format used in the local DB.
+ */
+function mapAPItoDBPerson(person: APIPerson): Person {
+  return {
+    id: person.login.uuid,
+    firstname: person.name.first,
+    lastname: person.name.last,
+    title: person.name.title,
+    email: person.email,
+    address1: `${person.location.street.name} ${person.location.street.number}`,
+    address2: `${person.location.postcode} ${person.location.city}`,
+    address3: person.location.country,
+    phone1: person.phone || person.cell,
+    phone2: !!person.phone ? person.cell : "",
+    thumbnail: person.picture?.thumbnail ?? "",
+    portrait: person.picture?.large ?? "",
+  }
+}
+
+/**
+ * Save person in store. Return promise that
+ * resolves on success and rejects on error
+ */
+function putPersonInStore(
+  store: IDBObjectStore,
+  person: APIPerson
+): Promise<void> {
+  let resolve: () => any,
+    reject: (ev: Event) => any,
+    promise = new Promise<void>((r, j) => {
+      resolve = r
+      reject = j
+    })
+  const op = store.put(mapAPItoDBPerson(person))
+  op.onsuccess = () => resolve()
+  op.onerror = ev => reject(ev)
+  return promise
+}
+
+/**
+ * Do the full db sync
+ */
+async function syncDB() {
+  if (
+    Date.now() <
+    +(localStorage.getItem(LS_LAST_SYNC_KEY) || 0) + TEN_MINUTES_MS
+  )
+    return
+  console.debug("BEGINNING SYNC")
+  let [db, apiPersons] = await Promise.all([getDB(), fetchAPIPersons()])
+  const store = db
+    .transaction(DB_OBJ_STORE, "readwrite")
+    .objectStore(DB_OBJ_STORE)
+  await Promise.all(apiPersons.map(person => putPersonInStore(store, person)))
+  localStorage.setItem(LS_LAST_SYNC_KEY, "" + Date.now())
+  console.debug("SYNC COMPLETE")
+}
+
+// Start an interval to sync every ten minutes, also check right await
+// on page load wether to sync
+setInterval(() => {
+  syncDB()
+}, TEN_MINUTES_MS)
+syncDB()
--- todo.md
+++ todo.md
@@ -1,8 +1,16 @@
-- [ ] Fetch stuff from api and render initial list
+# Plan
+
+- [x] Fetch and map users to our own format in indexeddb
+- [x] Regular syncing of db
+- [x] Indexing on names/emails for efficient search
+- [ ] Render initial list
+- [ ] Rough search experimentation
+- [ ] Rough detail page navigation
+- [ ] DB Error handling and compatibility checking for opening db
+- [ ] Event bus when api/db sync is ongoing, and error status
 - [ ] Initial design for mobile
 - [ ] Improve design for ipad
 - [ ] Improve design for desktop
-- [ ] Rough detail page navigation
 - [ ] Improve detail page navigation & design for mobile
 - [ ] Improve detail page navigation & design for ipad
 - [ ] Detail page navigation & design for desktop
@@ -10,3 +18,20 @@
 - [ ] Infinite scroll or pagination?
 - [ ] Sorting component
 - [ ] Lighthouse testing
+
+# Log
+
+Indexed db is set up and fetches. Will want to add error handling later, as well as an event bus for showing information in the UI when there is syncing. But first I want to
+just do some rough experimentation with searching/listing, now that I have the data.
+
+---
+
+Begain exploring the API and realized they throttle access. Decided I'd need to make sure user-db is synced locally. Will use indexeddb in order to index on name and email fields for efficient searching. It is supported on all relevant browsers.
+
+---
+
+Started using create-react-app with typescript template. Of the popular
+frameworks I'm most familiar with react. I also would like to show off
+familiarity with typescript. Create-react-app is just an easy way to get
+going. Cleaned out instrumentation, default icons and other stuff I don't
+expect to need.
